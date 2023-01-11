23d09a119ecd8597ab55e3b583cdb05884eb6482
Ling <1245670530@qq.com>
2023-01-12 00:57:25 +0800

    update

--- main.js
+++ main.js
@@ -1,39 +1,62 @@
 // 引入electron并创建一个Browserwindow
-const {app, Menu, BrowserWindow} = require('electron')
+const {app, Menu, BrowserWindow, globalShortcut, screen} = require('electron')
 const path = require('path')
 const url = require('url')
 // 保持window对象的全局引用,避免JavaScript对象被垃圾回收时,窗口被自动关闭.
 let mainWindow
+let fullScreenToggle = true;
 function createWindow () {
-mainWindow = new BrowserWindow({width: 800, height: 600})
- /* 
- * 加载应用----- electron-quick-start中默认的加载入口
- mainWindow.loadURL(url.format({
- pathname: path.join(__dirname, 'index.html'),
- protocol: 'file:',
- slashes: true
- }))
- */
- // 加载应用----适用于 react 项目
- mainWindow.loadURL('http://localhost:3000/');
- Menu.setApplicationMenu(null)
+    // Register a 'CommandOrControl+X' shortcut listener.
+    const ret = globalShortcut.register('CommandOrControl+F', () => {
+        console.log('CommandOrControl+F is pressed')
+        fullScreenToggle = !fullScreenToggle;
+        if (fullScreenToggle) {
+            mainWindow.restore();
+        } else {
+            mainWindow.minimize();
+        }
+    })
 
- mainWindow.webContents.on('before-input-event', (event, input) => {
-    if (input.control && input.shift && input.key.toLowerCase() === 'f') {
-        mainWindow.setFullScreen(true);
-        event.preventDefault()
-    } else if (input.alt && input.shift && input.key.toLowerCase() === 'f') {
-        mainWindow.setFullScreen(false)
-        event.preventDefault()
+    if (!ret) {
+        console.log('registration failed')
     }
-  })
+
+    // Check whether a shortcut is registered.
+    console.log(globalShortcut.isRegistered('CommandOrControl+F'))
+    mainWindow = new BrowserWindow({
+        width: screen.getPrimaryDisplay().size.width, 
+        height: screen.getPrimaryDisplay().size.width})
+    /* 
+    * 加载应用----- electron-quick-start中默认的加载入口
+    mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+    }))
+    */
+    // 加载应用----适用于 react 项目
+    mainWindow.loadURL('http://localhost:3000/');
+    Menu.setApplicationMenu(null)
+    mainWindow.setFullScreen(true);
+
+    // mainWindow.webContents.on('before-input-event', (event, input) => {
+    //     if (input.alt && input.shift && input.key.toLowerCase() === 'f') {
+    //         fullScreenToggle = !fullScreenToggle;
+    //         mainWindow.setFullScreen(fullScreenToggle);
+    //         event.preventDefault()
+    //     } else if (input.alt && input.shift && input.key.toLowerCase() === 'c') {
+    //         app.exit();
+    //     } else if (input.alt && input.shift && input.key.toLowerCase() === 'm') {
+    //         mainWindow.minmize();
+    //     }
+    // })
  
- // 打开开发者工具，默认不打开
- // mainWindow.webContents.openDevTools()
- // 关闭window时触发下列事件.
- mainWindow.on('closed', function () {
- mainWindow = null
- })
+    // 打开开发者工具，默认不打开
+    // mainWindow.webContents.openDevTools()
+    // 关闭window时触发下列事件.
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    })
 }
 // 当 Electron 完成初始化并准备创建浏览器窗口时调用此方法
 app.on('ready', createWindow)
@@ -49,4 +72,11 @@ app.on('activate', function () {
  if (mainWindow === null) {
  createWindow()
  }
+})
+app.on('will-quit', () => {
+    // Unregister a shortcut.
+    globalShortcut.unregister('CommandOrControl+F')
+
+    // Unregister all shortcuts.
+    globalShortcut.unregisterAll()
 })
--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.scss';
 import KeyBoard from './Key-Board/KeyBoard';
+import BubbleBackground from './BubbleBackground/BubbleBackground';
 import background from './Assets/heineken.jpg';
 
 function App() {
   return (
     <div className='App-container'>
-      <div className='App-background'>
-      <img alt='background' className='Image-background' src={background}></img>
-      <img alt='background' className='Image-background' src={background}></img>
-      <img alt='background' className='Image-background' src={background}></img>
-      <img alt='background' className='Image-background' src={background}></img>
-      </div>
+      <BubbleBackground></BubbleBackground>
       <div className='App-content'>
         <div className="App">
           <header className="App-header">
             <span className="App-title">React Test</span>
           </header>
-          <body className='App-body'>
-          <KeyBoard/>
-          </body>
+          <div className='App-body'>
+            <KeyBoard/>
+          </div>
         </div>
       </div>
       
